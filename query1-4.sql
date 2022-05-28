4388f8e93de2b2929aebd2dce365d827aa0963eb
G-SlBk <83564557+G-SlBk@users.noreply.github.com>
2022-05-28 10:47:52 +0000

    q4

---
+++ query1-4.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT address FROM address
+INNER JOIN store ON address.address_id=store.address_id
+INNER JOIN inventory ON store.store_id=inventory.store_id
+INNER JOIN film ON inventory.film_id=film.film_id
+WHERE title='TWISTED PIRATES';
